d62146249cd786bed9efcc00134a0309d095fad8
aq7642220 <2881362325@qq.com>
2019-05-13 18:02:03 +0800

    react代码提交

--- 杂项/my-react/src/index.js
+++ 杂项/my-react/src/index.js
@@ -1,19 +1,210 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'git
+import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import { Provider } from './react-redux'
 import CommentApp from './CommentApp'
 import './index.css'
 
 
 
 
+function createStore (reducer) {
+  let state = null
+  const listeners = []
+  const subscribe = (listener) => listeners.push(listener)
+  const getState = () => state
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+  dispatch({}) // 初始化 state
+  return { getState, dispatch, subscribe }
+}
 
-// ReactDOM.render(
-//   <CommentApp />,
-//   document.getElementById('root')
-// )
+const themeReducer = (state, action) => {
+  if (!state) return {
+    themeColor: 'red'
+  }
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return { ...state, themeColor: action.themeColor }
+    default:
+      return state
+  }
+}
+const store = createStore(themeReducer)
+
+const mapStateToProps = (state) => {
+  return {
+    themeColor: state.themeColor
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSwitchColor: (color) => {
+      dispatch({ type: 'CHANGE_COLOR', themeColor: color })
+    }
+  }
+}
+
+const connect = (mapStateToProps, mapDispatchToProps) => (WrappedComponent) => {
+  class Connect extends Component {
+    static contextTypes = {
+      store: PropTypes.object
+    }
+
+    constructor () {
+      super()
+      this.state = { allProps: {} }
+    }
+
+    componentWillMount() {
+      const { store } = this.context
+      this._updateProps()
+      store.subscribe(() => this._updateProps())
+    }
+
+    _updateProps () {
+      const { store } = this.context
+      let stateProps = mapStateToProps
+        ? mapStateToProps(store.getState(), this.props)
+        : {} // 防止 mapStateToProps 没有传入
+      let dispatchProps = mapDispatchToProps
+        ? mapDispatchToProps(store.dispatch, this.props)
+        : {} // 防止 mapDispatchToProps 没有传入
+      this.setState({
+        allProps: {
+          ...stateProps,
+          ...dispatchProps,
+          ...this.props
+        }
+      })
+    }
+
+    render () {
+      return <WrappedComponent {...this.state.allProps} />
+    }
+  }
+
+  return Connect
+}
+
+// class Header extends Component {
+//   static contextTypes = {
+//     store: PropTypes.object
+//   }
+//
+//   constructor () {
+//     super()
+//     this.state = { themeColor: '' }
+//   }
+//
+//   componentWillMount () {
+//     const { store } = this.context
+//     store.subscribe(() => this._updateThemeColor())
+//     this._updateThemeColor()
+//   }
+//
+//   _updateThemeColor () {
+//     const { store } = this.context
+//     const state = store.getState()
+//     this.setState({ themeColor: state.themeColor })
+//   }
+//
+//   render () {
+//     return (
+//       <h1 style={{ color: this.state.themeColor }}>React.js 小书</h1>
+//     )
+//   }
+// }
+
+class Header extends Component {
+  static propTypes = {
+    themeColor: PropTypes.string
+  }
+  render () {
+    return (
+      <h1 style={{ color: this.props.themeColor }}>React.js 小书</h1>
+    )
+  }
+}
+
+
+Header = connect(mapStateToProps)(Header)
 
 
 
+class ThemeSwitch extends Component {
+  static propTypes = {
+    themeColor: PropTypes.string,
+    onSwitchColor: PropTypes.func
+  }
 
- 
+  // dispatch action 去改变颜色
+  handleSwitchColor (color) {
+    if (this.props.onSwitchColor) {
+      this.props.onSwitchColor(color)
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <button
+          style={{ color: this.props.themeColor }}
+          onClick={this.handleSwitchColor.bind(this, 'red')}>Red</button>
+        <button
+          style={{ color: this.props.themeColor }}
+          onClick={this.handleSwitchColor.bind(this, 'blue')}>Blue</button>
+      </div>
+    )
+  }
+}
+
+
+ThemeSwitch = connect(mapStateToProps, mapDispatchToProps)(ThemeSwitch)
+
+
+
+class Content extends Component {
+  static propTypes = {
+    themeColor: PropTypes.string
+  }
+
+  render () {
+    return (
+      <div>
+        <p style={{ color: this.props.themeColor }}>React.js 小书内容</p>
+        <ThemeSwitch />
+      </div>
+    )
+  }
+}
+
+
+Content = connect(mapStateToProps)(Content)
+
+class Index extends Component {
+  // static childContextTypes = {
+  //   store: PropTypes.object
+  // }
+  // getChildContext () {
+  //   return { store }
+  // }
+  render () {
+    return (
+      <div>
+        <Header />
+        <Content />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Index />
+  </Provider>,
+  document.getElementById('root')
+)
